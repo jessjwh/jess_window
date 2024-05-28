efb94142934e02a275fbc21a12c923f161a772f5
jesshuang <jessjustgift@gmail.com>
2024-05-28 11:34:48 +0800

    modify

--- window/index.py
+++ window/index.py
@@ -1,5 +1,14 @@
-with open('names.txt', encoding='utf-8') as file:
-    content:str = file.read()
-names:list[str] = content.split()
-for name in names:
-    print(name)
+def get_names() -> list[str]:
+    with open('names.txt', encoding='utf-8') as file:
+        content:str = file.read()
+    names:list[str] = content.split()
+    return names
+    # for name in names:
+    #     print(name)
+
+# names:list[str] = get_names()
+# print(__name__)
+
+if __name__ == '__main__':
+    names:list[str] = get_names()
+    print(names)
