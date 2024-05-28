bd7016393bfca3602b49dc70831824c82c1be4a3
jesshuang <jessjustgift@gmail.com>
2024-05-28 14:45:07 +0800

    gui

--- window/index.py
+++ window/index.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 def get_names() -> list[str]:
     with open('names.txt', encoding='utf-8') as file:
@@ -12,12 +13,17 @@ def get_names() -> list[str]:
 # print(__name__)
 
 class Window(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title('My First GUI')
+    def __init__(self, title:str = "Hello!", **kwargs):
+        super().__init__(**kwargs)
+        self.title(title)
+        label:ttk.Label = ttk.Label(self, 
+                                    text='Hey Jess!',
+                                    font=('Arial', 30, 'bold'),
+                                    foreground='#f98')
+        label.pack(padx=100,pady=40)
 
 if __name__ == '__main__':
     names:list[str] = get_names()
     # print(names)
-    window:Window = Window()
+    window:Window = Window(title='My First GUI')
     window.mainloop()
