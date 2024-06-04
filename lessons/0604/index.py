8ab78c429f4fed786a2d69019fcb8e15607dd721
jesshuang <jessjustgift@gmail.com>
2024-06-04 15:32:29 +0800

    command_click

--- lessons/0604/index.py
+++ lessons/0604/index.py
@@ -12,9 +12,29 @@ class Window(ThemedTk):
         style.configure('Top.TFrame')
         style.configure('Top.TLabel',font=('Helvetica',25))
 
-        title_frame = ttk.Frame(self, style='Top.TFrame', borderwidth=2, relief='groove')
+        title_frame = ttk.Frame(self, style='Top.TFrame', borderwidth=1, relief='groove')
         ttk.Label(title_frame, text='AQI Display', style='Top.TLabel').pack(expand=True, fill='y')
-        title_frame.pack(ipadx=100, ipady=30, padx=10, pady=10)
+        title_frame.pack(ipadx=60, ipady=20, padx=10, pady=10)
+
+        func_frame = ttk.Frame(self, style='Top.TFrame', borderwidth=1, relief='groove')
+        ttk.Button(func_frame, text="AQI Best 5",command=self.click1).pack(side='left', expand=True)
+        ttk.Button(func_frame, text="AQI Worst 5",command=self.click2).pack(side='left', expand=True)
+        ttk.Button(func_frame, text="pm2.5 Best 5",command=self.click3).pack(side='left', expand=True)
+        ttk.Button(func_frame, text="pm2.5 Worst 5",command=self.click4).pack(side='left', expand=True)
+        func_frame.pack(ipadx=80, ipady=30, padx=10, pady=10)
+    
+    def click1(self):
+        print("click1")
+
+    def click2(self):
+        print("click2")
+
+    def click3(self):
+        print("click3")
+
+    def click4(self):
+        print("click4")
+
 
 def main():
     '''
