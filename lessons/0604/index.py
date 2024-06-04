313752df4f6514ff6c7eea1a8955a7edcce2d629
jesshuang <jessjustgift@gmail.com>
2024-06-04 14:54:59 +0800

    frame

--- lessons/0604/index.py
+++ lessons/0604/index.py
@@ -1,12 +1,20 @@
 from pprint import pprint
 import tkinter as tk
 from tkinter import ttk
+from ttkthemes import ThemedTk
 import tools
 
-class Window(tk.Tk):
+class Window(ThemedTk):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.title("AQI Display")
+        style = ttk.Style()
+        style.configure('Top.TFrame')
+        style.configure('Top.TLabel',font=('Helvetica',25))
+
+        title_frame = ttk.Frame(self, style='Top.TFrame', borderwidth=2, relief='groove')
+        ttk.Label(title_frame, text='AQI Display', style='Top.TLabel').pack(expand=True, fill='y')
+        title_frame.pack(ipadx=100, ipady=30, padx=10, pady=10)
 
 def main():
     '''
@@ -18,7 +26,7 @@ def main():
         data:list[dict] = tools.get_data(all_data)
         pprint(data)
     '''
-    window = Window()
+    window = Window(theme="yaru")
     window.mainloop()
 
 if __name__ == '__main__':
