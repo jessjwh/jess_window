ee507ec1e80da4842bd94438ec7b40eb1c76fe43
jesshuang <jessjustgift@gmail.com>
2024-06-06 14:16:22 +0800

    buttonbox

--- lessons/0606/index.py
+++ lessons/0606/index.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, Misc, Frame
 from ttkthemes import ThemedTk
 import tools
 from tkinter import messagebox
@@ -37,39 +37,62 @@ class Window(ThemedTk):
             data:list[dict] = tools.get_data(all_data)
             return data
 
-    def click1(self):
+
+    def update_data(self):
         if (tools.AQI.aqi_records is None) or (tools.AQI.update_time is None):
             tools.AQI.aqi_records = self.download_parse_data()
             tools.AQI.update_time = datetime.now()
-        elif((datetime.now()-tools.AQI.update_time).seconds >= 60*60):
+        elif((datetime.now()-tools.AQI.update_time).seconds >= 60 * 60):
             tools.AQI.aqi_records = self.download_parse_data()
             tools.AQI.update_time = datetime.now()
-    
+
+    def click1(self):
+        self.update_data()
         data:list[dict] = tools.AQI.aqi_records
         sorted_data:list[dict] = sorted(data, key=lambda value:value['aqi'])
         best5_aqi:list[dict] = sorted_data[:5]
         pprint(best5_aqi)
 
     def click2(self):
-        messagebox.showerror("Error","Error message")
-
+        self.update_data()
+    
     def click3(self):
+        self.update_data()
         messagebox.showwarning("Warning","Warning message")
 
     def click4(self):
+        self.update_data()
         ShowInfo(parent=self,title="This is a dialog.")
 
 class ShowInfo(Dialog):
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, parent:Misc, title:str | None = None):
+        super().__init__(parent=parent, title=title)
 
-    def body(self, master):
+    def body(self, master: Frame) -> Misc | None:
             text = tk.Text(self,height=8,font=('Helvetica',16),width=30)
             text.pack(padx=5,pady=5)
             text.insert(tk.INSERT,"Hello Jess")
             text.config(state='disabled')
             return None
+    
+    def apply(self) -> None:
+        '''
+        This will run when the user clicked the "ok" button in the jump out window.
+        '''
+        print("They clicked the 'ok' button.")
 
+    def buttonbox(self) -> None:
+        '''
+        Customized button
+        '''
+        box=tk.Frame(self)
+        self.ok_button = tk.Button(box, text='YASS!', width=10, command=self.ok, default=tk.ACTIVE)
+        self.ok_button.pack(pady=(20,30),ipady=10)
+        box.pack()
+    
+    def ok(self) -> None:
+        print("OK button was clicked!")
+        super().ok()
 
 def main():
     window = Window(theme="arc")
@@ -78,4 +101,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
