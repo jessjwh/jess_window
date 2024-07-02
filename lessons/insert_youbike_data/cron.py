d0b9c7cf96f51a1809e70f1fc8ad5d74ee0ca2c3
jesshuang <jessjustgift@gmail.com>
2024-07-02 16:54:23 +0800

    modify

--- lessons/insert_youbike_data/cron.py
+++ lessons/insert_youbike_data/cron.py
@@ -2,6 +2,7 @@ from crontab import CronTab
 import os
 cron = CronTab(user=True)
 path = os.path.abspath("./lessons2.py")
-job = cron.new(command=f"python '{path}'")
+job = cron.new(command=f"/opt/anaconda3/envs/venv2/bin/python '{path}'")
 job.minute.every(10)
+job.set_comment("Output hello world")
 cron.write()
